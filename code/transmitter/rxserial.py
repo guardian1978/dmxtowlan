0fbbb3935b5c5ec18ae0d8d1203df2983649d1e5
Carsten <guardian2@gmx.de>
2023-11-09 21:33:37 +0100

    add

---
+++ code/transmitter/rxserial.py
@@ -0,0 +1,17 @@
+# rxserial.py C.Wagner 9.11.23
+import socket
+import struct
+import sys
+import serial
+
+ser = serial.Serial("/dev/serial0",
+      250000,
+      bytesize=serial.EIGHTBITS,
+      parity=serial.PARITY_NONE,
+      stopbits=serial.STOPBITS_TWO)
+
+data = ser.read(128)
+print(daten)
+
+
+
