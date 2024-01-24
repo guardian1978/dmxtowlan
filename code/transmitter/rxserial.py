d60106b4210d11e820e1b55e4713bd78b8f7b56f
Carsten <guardian2@gmx.de>
2024-01-24 21:46:30 +0100

    update

--- code/transmitter/rxserial.py
+++ code/transmitter/rxserial.py
@@ -8,9 +8,10 @@ ser = serial.Serial("/dev/serial0",
       250000,
       bytesize=serial.EIGHTBITS,
       parity=serial.PARITY_NONE,
-      stopbits=serial.STOPBITS_ONE)
+      stopbits=serial.STOPBITS_TWO,
+      timeout=5)
 
-data = ser.read(16)
+data = ser.read(1024)
 print(data)
 
 
